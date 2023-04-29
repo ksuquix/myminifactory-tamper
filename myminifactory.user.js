70e4cfe10f19d68a41a67c9eaa5ef86ef3ef7d5e
Don Eisele <ksuquix@gmail.com>
2023-04-29 10:47:31 -0500

    add grab renders from lib page

--- myminifactory.user.js
+++ myminifactory.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name           Myminifactory Download assistant
 // @namespace      https://github.com/ksuquix/myminifactory-tamper
-// @version        0.0.5
+// @version        0.0.6
 // @description    An assist so you can see the projects you haven't gotten yet.
-// @include        https://www.myminifactory.com/object/*
+// @match        https://www.myminifactory.com/object/*
+// @match        https://www.myminifactory.com/library*
 // @require        http://code.jquery.com/jquery-1.12.4.min.js
 // @run-at         document-end
 // ==/UserScript==
@@ -32,9 +33,19 @@ function mygetall() {
     });
 }
 
+function mylibrenderall() {
+  let imout = [];
+  $("div.object-img a img").each(function() { imout.push($(this).attr("src"))})
+  navigator.clipboard.writeText("wget " + imout.join("\nwget "));
+  console.log("render in paste");
+  alert(`Pasted: ${imout.length}\n`+imout.join("\n"));
+}
+
 $(document).ready(function () {
+  $("button.MuiButton-containedPrimary").after('<button class="basic-button  red mylibrender"><i class="ico-download fa fa-arrow-down" aria-hidden="true"></i> Renders</button>')
+  $('button.mylibrender').click(mylibrenderall);
 	$("button.downloadButton").after('<button class="basic-button  red mydownloader"><i class="ico-download fa fa-arrow-down" aria-hidden="true"></i> Files</button>');
 	$('button.mydownloader').click(mygetall);
 	$("button.downloadButton").after('<button class="basic-button  red myrenders"><i class="ico-download fa fa-arrow-down" aria-hidden="true"></i> Renders</button>');
-  	$('button.myrenders').click(myrenderall);
+  $('button.myrenders').click(myrenderall);
 });
