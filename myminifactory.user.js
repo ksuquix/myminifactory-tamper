c11047489f7c5ebec1ec85c4e2ca7d52b5988d46
Don Eisele <ksuquix@gmail.com>
2023-04-28 22:28:14 -0500

    update

--- myminifactory.user.js
+++ myminifactory.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Myminifactory Download assistant
 // @namespace      https://github.com/ksuquix/myminifactory-tamper
-// @version        0.0.2
+// @version        0.0.3
 // @description    An assist so you can see the projects you haven't gotten yet.
 // @include        https://www.myminifactory.com/object/*
 // @require        http://code.jquery.com/jquery-1.12.4.min.js
@@ -9,15 +9,7 @@
 // ==/UserScript==
 
 function mygetall() {
-  // Fetch all downloads (on specific object page)
-  jQuery
-    .parseJSON($(".cont-download script").text())
-    .archives.forEach(function (lnk) {
-      console.log(lnk.download_url);
-      window.open(lnk.download_url);
-    });
-
-  // drop wgets for images into paste buffer
+  // drop wgets for images into paste buffer (do first while document is focused)
   impath = $('meta[property="og:image"]').attr("content").match(/.*\//)[0];
   imout = [];
   $("img")
@@ -26,6 +18,15 @@ function mygetall() {
     .filter((s) => s.includes(impath))
     .forEach((x) => imout.push(x.replace("70X70", "720x720")));
   navigator.clipboard.writeText("wget " + imout.join("\nwget "));
+
+  
+  // Fetch all downloads (on specific object page)
+  jQuery
+    .parseJSON($(".cont-download script").text())
+    .archives.forEach(function (lnk) {
+      console.log(lnk.download_url);
+      window.open(lnk.download_url);
+    });
 }
 
 $(document).ready(function () {
