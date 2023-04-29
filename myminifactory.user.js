0b01f23bbaac04c0ee3fa0ed3eb0a56565a6d926
Don Eisele <ksuquix@gmail.com>
2023-04-28 22:34:17 -0500

    update

--- myminifactory.user.js
+++ myminifactory.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name           Myminifactory Download assistant
 // @namespace      https://github.com/ksuquix/myminifactory-tamper
-// @version        0.0.3
+// @version        0.0.4
 // @description    An assist so you can see the projects you haven't gotten yet.
 // @include        https://www.myminifactory.com/object/*
 // @require        http://code.jquery.com/jquery-1.12.4.min.js
 // @run-at         document-end
 // ==/UserScript==
 
-function mygetall() {
+function myrenderall() {
   // drop wgets for images into paste buffer (do first while document is focused)
   impath = $('meta[property="og:image"]').attr("content").match(/.*\//)[0];
   imout = [];
@@ -19,7 +19,7 @@ function mygetall() {
     .forEach((x) => imout.push(x.replace("70X70", "720x720")));
   navigator.clipboard.writeText("wget " + imout.join("\nwget "));
 
-  
+function mygetall() {
   // Fetch all downloads (on specific object page)
   jQuery
     .parseJSON($(".cont-download script").text())
@@ -30,8 +30,8 @@ function mygetall() {
 }
 
 $(document).ready(function () {
-  $("button.downloadButton").after(
-    '<button class="basic-button  red mydownloader"><i class="ico-download fa fa-arrow-down" aria-hidden="true"></i> All</button>'
-  );
-  $('button.mydownloader').click(mygetall);
+	$("button.downloadButton").after('<button class="basic-button  red mydownloader"><i class="ico-download fa fa-arrow-down" aria-hidden="true"></i> Files</button>');
+	$('button.mydownloader').click(mygetall);
+	$("button.downloadButton").after('<button class="basic-button  red myrenders"><i class="ico-download fa fa-arrow-down" aria-hidden="true"></i> Renders</button>');
+  	$('button.myrenders').click(myrenderall);
 });
